90e8b8f99da8087612f939809aa315077b7ee86b
Eyal <eyal.heinemann@gmail.com>
2025-12-14 12:53:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -10,6 +11,9 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothis eill be:', name_on_order)
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Get the current credentials
 cnx = st.connection("snowflake")
